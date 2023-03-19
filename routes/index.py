3e570bf0bc4dd41487a0b0685f57ac577eab09a2
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-03-19 23:18:13 +0300

    25

--- routes/index.py
+++ routes/index.py
@@ -7,22 +7,31 @@ from . import routes
 # Главная страница
 @routes.route("/")
 def index():
+    # Получаем все посты из базы данных
     db_sess = db_session.create_session()
     posts = db_sess.query(Post).filter(Post.is_archived == False).all()
+
+    # Возвращаем шаблон с постами
     return render_template("index.html", posts=posts, title="Все посты")
 
 
 # Страницы с постами про потеряшки
 @routes.route("/lost")
 def lost():
+    # Получаем все посты с потеряшками из базы данных
     db_sess = db_session.create_session()
     posts = db_sess.query(Post).filter(Post.is_archived == False, Post.is_found == False).all()
+
+    # Возвращаем шаблон с постами
     return render_template("index.html", posts=posts, title="Потеряно")
 
 
 # Страницы с постами про найденные вещи
 @routes.route("/found")
 def found():
+    # Получаем все посты с найденными вещами из базы данных
     db_sess = db_session.create_session()
     posts = db_sess.query(Post).filter(Post.is_archived == False, Post.is_found == True).all()
+
+    # Возвращаем шаблон с постами
     return render_template("index.html", posts=posts, title="Найдено")
