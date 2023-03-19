d0ac715d69cde9d88fb4ff8e21296bc876c7f343
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-03-19 23:15:15 +0300

    22

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask
-from data import db_session
+from routes import *
 
+# Создание приложения и подключение роутов
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.register_blueprint(routes)
+
+# Создание менеджера для аутентификации
+login_manager = LoginManager()
 
 
+# Функция для загрузки пользователя
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+
+    return db_sess.query(User).get(user_id)
+
+
+# Функция для запуска приложения
 def main():
+    login_manager.init_app(app)
     db_session.global_init("db/database.sqlite")
-    app.run()
+    app.run(port=80)
 
 
+# Запуск приложения
 if __name__ == '__main__':
     main()
