7a92535d02db96342fbb790324f47ce706900d7c
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-03-20 09:54:24 +0300

    39

--- data/posts.py
+++ data/posts.py
@@ -3,17 +3,35 @@ import sqlalchemy
 from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 
+# Таблица связи между пользователями и лайками на посты
+liked_users_table = sqlalchemy.Table(
+    'liked_users_table',
+    SqlAlchemyBase.metadata,
+    sqlalchemy.Column('users', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('users.id')),
+    sqlalchemy.Column('posts', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('posts.id'))
+)
 
-class News(SqlAlchemyBase):
-    __tablename__ = 'news'
+
+# Класс поста
+class Post(SqlAlchemyBase):
+    __tablename__ = 'posts'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
 
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    is_found = sqlalchemy.Column(sqlalchemy.Boolean)
 
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    is_archived = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
+
+    images = orm.relationship("PostImage", back_populates='post')
+
+    liked_users = orm.relation('User', secondary='liked_users_table', back_populates="liked_posts")
+
+    comments = orm.relation("Comment", back_populates='post')
